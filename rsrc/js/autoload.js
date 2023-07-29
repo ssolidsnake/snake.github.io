f6f03fdc2bba7be9fa0b7ef8a783a885aac9dc80
solid snake <140867595+ssolidsnake@users.noreply.github.com>
2023-07-28 22:19:01 -0600

    Create autoload.js

---
+++ rsrc/js/autoload.js
@@ -0,0 +1,44 @@
+const appAutoload=(function(w){
+	let value=fname=i=el=uri=null;
+	const rsrc={
+		"js":[
+			"jquery.min.js",
+			"bootstrap.min.js",
+			""
+		],
+		"css":[
+			"bootstrap.min.css",
+			"",
+		],
+	};
+	for(const key in rsrc){
+		if(rsrc.hasOwnProperty(key))
+		{
+			value=rsrc[key];
+			value.forEach((fname,i)=>{
+				if(fname!==null && fname!==''){
+					uri="/rsrc/"+key+'/'+fname;
+					if(key==='css')
+					{
+						el=document.createElement("link");
+						el.href=uri;
+						el.rel='stylesheet';
+						document.head.appendChild(el);
+					}			
+					else if(key==='js')
+					{
+						el=document.createElement("script");
+						el.src=uri;
+						document.body.appendChild(el);
+					}
+				}
+			});
+		}
+	}
+})(window,undefined);
+/*
+ * Author:ssolidsnake
+ * Threads:https://www.threads.net/@solidsnakee_
+ * Nitter:https://nitter.net/solidsnakee_
+ * X:https://x.com/solidsnakee_
+ * */

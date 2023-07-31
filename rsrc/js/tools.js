3d478ef8cee1675294a399cf48d45fbe9e336040
solid snake <140867595+ssolidsnake@users.noreply.github.com>
2023-07-31 07:42:43 -0600

    Update tools.js

--- rsrc/js/tools.js
+++ rsrc/js/tools.js
@@ -1,6 +1,12 @@
 var colorsInPalette=0;
 const tool=(function(w,undefined){
 	function generateColorPalette(){
+		let el=i=null;
+		while(i<1000){
+			el=helper.generateColorItem('button');
+			document.getElementById('colorPalette').appendChild(el);
+			i++;
+		}
 		handler.infinityScroll(function(){
 			i=0;
 			while(i<1000){
